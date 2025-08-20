7f4037a0b510f7ca4908cba9c8e77c11ed4f55c2
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-08-20 21:38:08 +0530

    Type Casting

---
+++ type casting.py
@@ -0,0 +1,16 @@
+a = "HI" 
+b = 35
+c = 89.8
+d = False
+# Before type casting
+print ("Before Type Casting")
+print (a,type(a))
+print (b,type(b))
+print (c,type(c))
+print (d,type(d))
+#After type casting
+print ("After Type Casting")
+b = float(b)
+print (b,type(b))
+c = int(c)
+print (c,type(c))
